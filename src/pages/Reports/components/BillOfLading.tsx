d9d240145d665a70bbb170f66b43c82cb3e3f11b
jasondhoman <jasondhoman@gmail.com>
2023-06-13 15:23:00 -0500

    correct total cost calc

--- src/pages/Reports/components/BillOfLading.tsx
+++ src/pages/Reports/components/BillOfLading.tsx
@@ -57,12 +57,6 @@ const BillOfLading: React.FC = () => {
             elevation={0}
             className="p-3 w-100"
           >
-            <input
-              type="date"
-              onChange={(e) =>
-                console.log(typeof e.target.value, e.target.value)
-              }
-            />
             <Grid
               container
               direction="row"
